72a642ffbb5e5210fcd3205fb61335b5948a7c95
rkorimba <korimbacus.riad@gmail.com>
2018-05-28 11:46:23 +0200

    Montrer le joueur

--- ColorBee/Scene/GameScene.swift
+++ ColorBee/Scene/GameScene.swift
@@ -11,9 +11,25 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let cameraNode = SKCameraNode()
+    
     override func didMove(to view: SKView) {
         
-     
+        physicsWorld.gravity.dy = -5
+        
+        camera = cameraNode
+        addChild(cameraNode)
+        cameraNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
+    
+        let joueur = SKShapeNode(circleOfRadius: 25)
+        joueur.fillColor = .blue
+        joueur.strokeColor = .blue
+        joueur.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        let physicsBody = SKPhysicsBody(circleOfRadius: 25)
+        physicsBody.affectedByGravity = true
+        physicsBody.mass = 1.5
+        joueur.physicsBody = physicsBody
+        addChild(joueur)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
