88f2a749dc4994173fb9514ae8c49d6724cf6c44
rkorimba <korimbacus.riad@gmail.com>
2018-05-28 11:39:41 +0200

    Creation projet

---
+++ ColorBee/Scene/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  ColorBee
+//
+//  Created by Riad Korimbacus on 28/05/2018.
+//  Copyright © 2018 Riad Korimbacus. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+     
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+       
+    }
+}
